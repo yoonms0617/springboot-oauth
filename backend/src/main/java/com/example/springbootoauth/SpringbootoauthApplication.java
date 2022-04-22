2ea090dbcf89abf869e48728a6cb8bbd04284060
yoonms0617 <yoonminwater@gmail.com>
2022-04-22 10:05:01 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/springbootoauth/SpringbootoauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootoauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootoauthApplication.class, args);
+    }
+
+}
