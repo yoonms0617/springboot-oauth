263a2db4d1735e0a75f6ee88a2de981eb8688dd1
yoonms0617 <yoonminwater@gmail.com>
2022-04-24 18:04:55 +0900

    feat: 웹 설정 작성

---
+++ backend/src/main/java/com/example/springbootoauth/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.springbootoauth.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${web.cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+
+}
